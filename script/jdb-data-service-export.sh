58bcdc76b041a7f2e80aba32efb97e947d851bba
关鹏 <guanpeng@hffss.com>
2019-11-01 09:19:05 +0800

    convert docker start

---
+++ script/jdb-data-service-export.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+PROJECT_NAME=jenkins-china-mirrors-convert
+PROJECT_VERSION=1.0.0-SNAPSHOT
+BASE_DIR=$(dirname "$PWD")
+cd $BASE_DIR
+mvn docker:build
+docker stop $PROJECT_NAME
+docker rm $PROJECT_NAME
+docker run -p 11302:11302--name=$PROJECT_NAME -it -d --restart=always $PROJECT_NAME:$PROJECT_VERSION
+docker images|grep none|awk '{print $3}'|xargs docker rmi
